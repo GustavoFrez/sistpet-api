5086bb9b70f33b3b6fd67b4609e47a346371ecae
Gustavo Frez <gustavo.frez@conexasaude.com.br>
2022-06-29 21:29:41 -0300

    Config BD

--- database/connection.js
+++ database/connection.js
@@ -1,30 +1,40 @@
-const kenx = require('knex')({
-    client:'mysql2',
-    connection:{
-        host:'localhost',
-        user:'root',
-        password:'Conexa@saude321',
-        database:'sistPet',
-    }
-});     
+const kenx = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b6068e5982aae8",
+    password: "6d6a3864",
+    database: "heroku_e4c12dba8a3b188",
+  },
+});
 
 //SEQUELIZE
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-const Connection = new Sequelize('sistPet','root','Conexa@saude321',{
-    host:'localhost',
-    dialect:'mysql',
+const Connection = new Sequelize(
+  "heroku_e4c12dba8a3b188",
+  "b6068e5982aae8",
+  "6d6a3864",
+  {
+    host: "us-cdbr-east-06.cleardb.net",
+    dialect: "mysql",
     define: {
-        timestamps: false,
-        freezeTableName: true
-    }
-})
+      timestamps: false,
+      freezeTableName: true,
+    },
+  }
+);
 
-Connection.authenticate().then(()=>{console.log('Conexão com o banco estabelicida')})
-.catch(err => {console.log(`Erro ao se conectar com banco: \n ${err}`)})
+Connection.authenticate()
+  .then(() => {
+    console.log("Conexão com o banco estabelicida");
+  })
+  .catch((err) => {
+    console.log(`Erro ao se conectar com banco: \n ${err}`);
+  });
 
 module.exports = {
-    database : kenx,
-    db : Connection,
-    Sequelize : Sequelize,
-}
+  database: kenx,
+  db: Connection,
+  Sequelize: Sequelize,
+};
